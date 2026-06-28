6ad339ab1d5b6367760278ec861ebe0730de8c91
maruimoka <134837681+maruimoka@users.noreply.github.com>
2026-06-28 08:47:00 +0700

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://xxxxxxxx.supabase.co";
+
+const SUPABASE_KEY = "YOUR_ANON_KEY";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
