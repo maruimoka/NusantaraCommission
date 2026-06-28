18fcf155f3eb32a6e0aabbb44524cb5b57bd44c8
maruimoka <134837681+maruimoka@users.noreply.github.com>
2026-06-28 09:40:26 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://yabiewxixiicbbojqsvb.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_2uGvxumwsc15Q8Sl07BuVw_NWHbOw0t";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
