1d8da56d4f186f5785ba3d37911d9ba2cc740fb4
maruimoka <134837681+maruimoka@users.noreply.github.com>
2026-07-02 10:34:16 +0700

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -156,6 +156,45 @@ window.location.href =
 
 }
 
+// HERO BANNER
+// =========================
+// HERO SLIDER
+// =========================
+
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide =>
+        slide.classList.remove("active")
+    );
+
+    dots.forEach(dot =>
+        dot.classList.remove("active")
+    );
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+
+}
+
+setInterval(nextSlide, 4000);
+
 // =========================
 // INIT
 // =========================
